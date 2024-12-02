2a57a88ef129733238e261212e6e7680ca9af6ad
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-12-02 20:07:49 +0100

    Update Day Two

--- Day2/script.js
+++ Day2/script.js
@@ -5,21 +5,17 @@ fetch("input.txt")
     let resultat = 0
 
     for(let i = 0; i < lignes.length; i++) {
-        let lignesSplit = (lignes[i].split(" "))
-        let lignesSplitInt = lignesSplit.map(Number)
+        let lignesSplit = lignes[i].split(" ").map(Number)
         let compteur = 0
-        for(let j = 0; j < lignesSplitInt.length-1; j++) {
-            if(j === lignesSplitInt.length){
-                lignesSplitInt[j] -= "\r"
-            }
-            if(lignesSplitInt[j] < lignesSplitInt[j+1] && ((lignesSplitInt[j+1]) - lignesSplitInt[j]) <= 3){
+        for(let j = 0; j < lignesSplit.length-1; j++) {
+            if(lignesSplit[j] < lignesSplit[j+1] && ((lignesSplit[j+1]) - lignesSplit[j]) <= 3){
                 compteur++
             }
-            else if(lignesSplitInt[j] > lignesSplitInt[j+1] && ((lignesSplitInt[j]) - lignesSplitInt[j+1]) <= 3){
+            else if(lignesSplit[j] > lignesSplit[j+1] && ((lignesSplit[j]) - lignesSplit[j+1]) <= 3){
                 compteur++
             }
         }
-        if(compteur === lignesSplitInt.length) {
+        if(compteur === lignesSplit.length - 1) {
             resultat++
         }
     }
