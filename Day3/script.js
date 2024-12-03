72361f44ddf2dd73d38f1d77d55427d7b12d10e9
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-12-03 18:05:00 +0100

    Update Day Three

--- Day3/script.js
+++ Day3/script.js
@@ -1,14 +1,22 @@
 const regex1 = /mul\(\d+,\d+\)/g
-const regex2 = /\d+,\d+/
+const regex2 = /\d+,\d+/g
 
 fetch("input.txt")
 .then(response => response.text())
 .then(data => {
     const tabRegex = data.match(regex1)
-    for(let i = 0; i < tabRegex.length; i++){
-        let ligneTabRegex = tabRegex[i].match(regex2)
-        console.log(ligneTabRegex)
+    const tabRegexNbr = []
+    for(let i = 0; i < tabRegex.length; i++) {
+        tabRegexNbr.push(tabRegex[i].match(regex2))
     }
-    
 
+    let resultat = 0
+
+    for(let i = 0; i < tabRegexNbr.length; i++) {
+        const paire = tabRegexNbr[i][0]
+        const [nombre1, nombre2] = paire.split(",").map(Number)
+        const produit = nombre1 * nombre2
+        resultat += produit
+    }
+    console.log(resultat)
 })
