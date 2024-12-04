672b4c2bc99103b9b4f294dd34533346f6dd4d1c
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-12-04 19:47:03 +0100

    Update script.js

--- Day4/script.js
+++ Day4/script.js
@@ -2,40 +2,37 @@ fetch("input.txt")
 .then(response => response.text())
 .then(data => {
     const lignes = data.split("\n")
-    const lettres = []
-    for(let i = 0; i < lignes.length; i++) {
-        lettres.push(lignes[i].split(""))
-    }
+    const lettres = lignes.map(ligne => ligne.split(""))
 
     let compteur = 0
     
-    for(let i = 0; i < lignes.length-3; i++){
-        for(let j = 0; j < lettres.length; j++){
-            if(lignes[i][j] === "X" && lignes[i][j+1] === "M" && lignes[i][j+2] === "A" && lignes[i][j+3] === "S"){ //horizontale xmas
-                compteur++
-            }
-            if(lignes[i][j] === "S" && lignes[i][j+1] === "A" && lignes[i][j+2] === "M" && lignes[i][j+3] === "X"){ //horizontal smax
-                compteur++
-            }
-            if(lignes[i][j] === "X" && lignes[i+1][j] === "M" && lignes[i+2][j] === "A" && lignes[i+3][j] === "S"){ //vertical xmas
-                compteur++
-            }
-            if(lignes[i][j] === "S" && lignes[i+1][j] === "A" && lignes[i+2][j] === "M" && lignes[i+3][j] === "X"){ //vertical smax
-                compteur++
-            }
-            if(lignes[i][j] === "X" && lignes[i+1][j+1] === "M" && lignes[i+2][j+2] === "A" && lignes[i+3][j+3] === "S"){ //diag droite xmas
-                compteur++
-            }
-            if(lignes[i][j] === "S" && lignes[i+1][j+1] === "A" && lignes[i+2][j+2] === "M" && lignes[i+3][j+3] === "X"){ //diag droite smax
-                compteur++
-            }
-            if(lignes[i][j] === "X" && lignes[i+1][j-1] === "M" && lignes[i+2][j-2] === "A" && lignes[i+3][j-3] === "S"){ //diag gauche xmas
-                compteur++
-            }
-            if(lignes[i][j] === "S" && lignes[i+1][j-1] === "A" && lignes[i+2][j-2] === "M" && lignes[i+3][j-3] === "X"){ //diag gauche smax
-                compteur++
-            }
+    for (let i = 0; i < lignes.length - 3; i++) {
+        for (let j = 0; j < lettres.length - 3; j++) {
+          if (lettres[i][j] === 'X' && lettres[i][j+1] === 'M' && lettres[i][j+2] === 'A' && lettres[i][j+3] === 'S') {
+            compteur++
+          }
+          if (j >= 3 && lettres[i][j] === 'X' && lettres[i][j-1] === 'M' && lettres[i][j-2] === 'A' && lettres[i][j-3] === 'S') {
+            compteur++
+          }
+          if (lettres[i][j] === 'X' && lettres[i+1][j] === 'M' && lettres[i+2][j] === 'A' && lettres[i+3][j] === 'S') {
+            compteur++
+          }
+          if (i >= 3 && lettres[i][j] === 'X' && lettres[i-1][j] === 'M' && lettres[i-2][j] === 'A' && lettres[i-3][j] === 'S') {
+            compteur++
+          }
+          if (lettres[i][j] === 'X' && lettres[i+1][j+1] === 'M' && lettres[i+2][j+2] === 'A' && lettres[i+3][j+3] === 'S') {
+            compteur++
+          }
+          if (i >= 3 && j >= 3 && lettres[i][j] === 'X' && lettres[i-1][j-1] === 'M' && lettres[i-2][j-2] === 'A' && lettres[i-3][j-3] === 'S') {
+            compteur++
+          }
+          if (i >= 3 && lettres[i][j] === 'X' && lettres[i+1][j-1] === 'M' && lettres[i+2][j-2] === 'A' && lettres[i+3][j-3] === 'S') {
+            compteur++
+          }
+          if (i >= 3 && j >= 3 && lettres[i][j] === 'X' && lettres[i-1][j+1] === 'M' && lettres[i-2][j+2] === 'A' && lettres[i-3][j+3] === 'S') {
+            compteur++
+          }
         }
-    }
-    console.log(compteur)
-})
+      }
+      console.log(compteur)
+    })
