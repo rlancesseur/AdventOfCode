2ad9f0713e489df3e564027a1f2d9618aa1ad5c9
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-12-05 21:29:57 +0100

    Update Day Five

--- Day5/script.js
+++ Day5/script.js
@@ -4,10 +4,19 @@ const regex2 = /([0-9]+(,[0-9]+)+)/g
 fetch("input.txt")
 .then(response => response.text())
 .then(data => {
-    const paires = data.match(regex1)
-    const suites = data.match(regex2)
-    const suiteNbr = suites.map(suite => suite.split(","))
+    const paires = data.match(regex1).map(paire => paire.split("|"))
+    const suites = data.match(regex2).map(suite => suite.split(","))
+    let sommeMilieu = 0
 
-    console.log(suiteNbr[0][2])
+    for (let i = 0; i < suites.length; i++) {
+        for(let j = 0; j < suites[i].length ; j++) {
+            if(suites[i][j] === paires[i][0]) {
+                //if(paires[i][1] === suites[i][j+1]){}
+            }
+        }
+    }
+
+    //console.log(sommeMilieu)
+    //A finir
 })
 
