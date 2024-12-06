4ede4b234206fced1d3f7783e3774217fcc56e0c
Rémi Lancesseur <56547982+rlancesseur@users.noreply.github.com>
2024-12-06 19:47:19 +0100

    Add Day Six

---
+++ Day6/script.js
@@ -0,0 +1,99 @@
+fetch("input.txt")
+.then(response => response.text())
+.then(data => {
+    const matrice = data.split("\n").map(ligne => ligne.split(""))
+
+    const OuEstPositionActuelle = (matrice) => {
+        for(let i = 0; i < matrice.length; i ++) {
+            for(let j = 0; j < matrice[i].length; j++) {
+                if(matrice[i][j] === "^"){
+                    return {ligne: i, colonne: j}
+                }
+            }
+        }
+    }
+
+    const positionActuelle = OuEstPositionActuelle(matrice)
+    let resultat = 0
+
+    const haut = (matrice, positionActuelle) => {
+        const ligne = positionActuelle.ligne
+        const colonne = positionActuelle.colonne
+        
+        const valeurDessus = matrice[ligne - 1][colonne]
+        while(valeurDessus != "#" && ligne > 0) {
+            matrice[ligne - 1][colonne] = "^"
+            matrice[ligne][colonne] = "."
+            resultat++
+            haut(matrice, [ligne - 1, colonne])
+        }
+        if (ligne === 0) {
+            return resultat
+        }
+        else {
+            droite()
+        } 
+    }
+
+    const droite = (matrice, positionActuelle) => {
+        const ligne = positionActuelle.ligne
+        const colonne = positionActuelle.colonne
+        
+        const valeurDroite = matrice[ligne][colonne + 1]
+        while(valeurDroite != "#" && colonne < matrice[0].length) {
+            matrice[ligne][colonne + 1] = "^"
+            matrice[ligne][colonne] = "."
+            resultat++
+            droite(matrice, [ligne, colonne + 1])
+        }
+        if (colonne === matrice[0].length) {
+            return resultat
+        }
+        else {
+            bas()
+        }
+    }
+
+    const bas = (matrice, positionActuelle) => {
+        const ligne = positionActuelle.ligne
+        const colonne = positionActuelle.colonne
+        
+        const valeurBas = matrice[ligne + 1][colonne]
+        while(valeurBas != "#" && ligne < matrice.length) {
+            matrice[ligne +1][colonne] = "^"
+            matrice[ligne][colonne] = "."
+            resultat++
+            bas(matrice, [ligne + 1, colonne])
+        }
+        if (ligne === matrice.length) {
+            return resultat
+        }
+        else {
+            gauche()
+        } 
+    }
+
+    const gauche = (matrice, positionActuelle) => {
+        const ligne = positionActuelle.ligne
+        const colonne = positionActuelle.colonne
+
+        const valeurGauche = matrice[ligne][colonne - 1]
+        while(valeurGauche != "#" && colonne > 0) {
+            matrice[ligne][colonne - 1] = "^"
+            matrice[ligne][colonne] = "."
+            resultat++
+            gauche(matrice, [ligne, colonne - 1])
+        }
+        if (colonne === 0) {
+            return resultat
+        }
+        else {
+            haut()
+        }
+    }
+
+    haut()
+    //A finir
+
+    console.log(resultat)
+})
